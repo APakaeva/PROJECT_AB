9054884f4ef0a713a5fb5cbac5ece6b4c5e75a74
Alissa-1 <alena.cheremukhina@gmail.com>
2022-07-18 23:28:25 +0300

    fixed #1, fixed #2, fixed #3; Methods containsGames/addPlayedTime/getSumPlayedTime fixed

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -1,20 +1,17 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class GameStoreTest {
 
-    @Disabled
     @Test
     public void shouldAddGame() {
 
-//        GameStore store = new GameStore();
-//        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
-//
-//        assertTrue(store.containsGame(game));
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        assertTrue(store.containsGame(game));
     }
 
     // другие ваши тесты
